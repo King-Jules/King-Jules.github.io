be951e19b474c8deb9afbe053d9f231b3513607a
King-Jules <jbrillon95@gmail.com>
2015-09-23 21:37:49 -0400

    test

---
+++ js/jquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$( "#pic" ).hover(function() {
+    	$( this ).addClass( "project-hover" );
+ 	}, function() {
+    	$( this ).removeClass( "project-hover" );
+ 	}
+});
